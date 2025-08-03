ca9fa53e8e20e3079864785cd988f1c0f7dd8956
eniskorkut <enis_korkut@hotmail.com>
2025-08-03 16:56:46 +0300

    Mesajınız buraya gelecek

---
+++ ApiProjeKampi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori ekleme işlemi başarılı");
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori silme işlemi başarılı");
+        }
+
+        [HttpGet("GetCategory")]// normalde aynı controller da birden fazla get olamaz ama buna özel isim verdik. özel isim metot ismi ile aynı olmalı!!!!
+        public IActionResult GetCategory(int id)
+        {
+
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori güncelleme işlemi başarılı");
+        }
+
+    }
+    
+}
+
---
+++ ApiProjeKampi.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ChefsController : ControllerBase
+    {
+
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IActionResult ChefList()
+        {
+            var values = _context.Chefs.ToList();
+            return Ok(values);
+        }
+
+        // PUT api/values/5
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return Ok("Şef sisteme başarıyla eklendi");
+        }
+
+        
+        [HttpDelete]
+        public IActionResult DeleteChef(int id)
+        {
+           var value = _context.Chefs.Find(id);
+            _context.Chefs.Remove(value);
+            return Ok("Şef silme işlemi başarılı");
+        }
+
+        [HttpGet("GetChef")]
+        public IActionResult GetChef(int id)
+        {   
+            return Ok(_context.Chefs.Find(id));
+        }
+
+        [HttpPut]
+        public IActionResult UpdateChef(Chef chef)
+        {
+            _context.Chefs.Update(chef);
+            _context.SaveChanges();
+            return Ok("Şef güncelleme işlemi başarılı");
+        }
+    }
+}
+
